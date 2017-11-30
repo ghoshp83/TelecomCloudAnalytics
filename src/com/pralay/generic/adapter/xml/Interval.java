70b92e7296c0b9b856d9b9391c8220b1486cbdf2
Pralay Ghosh <ghoshp83@users.noreply.github.com>
2017-11-30 13:35:03 +0530

    Update Interval.java

--- src/com/pralay/generic/adapter/xml/Interval.java
+++ src/com/pralay/generic/adapter/xml/Interval.java
@@ -1,9 +1,6 @@
 package com.pralay.generic.adapter.xml;
 
 
-/**
- * Created by edmyvar on 10/20/2015.
- */
 public enum Interval {
     IVL_1_MIN(0, 60 * 1000L), IVL_5_MIN(1, 5 * 60 * 1000L), IVL_15_MIN(2, 15 * 60 * 1000L), IVL_30_MIN(3,
             30 * 60 * 1000L), IVL_1_HOUR(4, 1 * 60 * 60 * 1000L), IVL_1_DAY(5, 24 * 60 * 60 * 1000L);
